216e2483a50a6003dbd3ed17036d27819b0e354f
asentaja <Asentaja@BC-36ZV7S2.bc.local>
2020-03-10 22:49:36 +0200

    HTTP Methods & RESTful APIs

---
+++ server.js
@@ -0,0 +1,52 @@
+const http = require('http');
+
+const todos = [
+    {id: 1, text: 'Todo One'},
+    {id: 2, text: 'Todo Two'},
+    {id: 3, text: 'Todo Three'}
+]
+
+const server = http.createServer((req, res) => {
+
+    const {method, url} = req;
+
+    console.log(req.headers.authorization);
+
+    let body = []
+
+    req.on('data', e => {
+        body.push(e);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+
+        let status = 404;
+        const response = {
+            success: false,
+            data: null
+        }
+
+        if(method === 'GET' && url === '/todos') {
+            status = 200;
+            response.success = true;
+            response.data = todos;
+        }
+
+
+        res.writeHead(status, {
+            'Content-Type': 'application/json',
+            'X-Powered-By':  'Node.js'
+        });
+
+        res.end(
+            JSON.stringify(response)
+    
+        );
+    
+    })
+
+
+})
+
+const PORT = 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
